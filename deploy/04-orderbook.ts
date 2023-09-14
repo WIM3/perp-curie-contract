1c6b3655fd2ff2d0a6873efb81922fbda358f987
Fran <francisco.palacios@external.wolterskluwer.com>
2023-09-14 22:51:49 +0100

    feat: implement orderbook deployment

---
+++ deploy/04-orderbook.ts
@@ -0,0 +1,39 @@
+import { network } from "hardhat"
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { HARDHAT_CHAINID, isDevelopmentChain } from "../helper.hardhat.config"
+import { verify } from "../scripts/verify"
+
+const deploy = async (hre: HardhatRuntimeEnvironment) => {
+    const { log, get, deploy } = hre.deployments
+    const { deployer } = await hre.getNamedAccounts()
+    const chainId = network.config.chainId || HARDHAT_CHAINID
+    log("#########################")
+    log(`# Deploying Orderbook Contract to: ${chainId} ...`)
+
+    const marketRegistry = await get("MarketRegistry")
+    const orderbookContract = await deploy("OrderBook", {
+        from: deployer,
+        args: [],
+        log: true,
+        proxy: {
+            owner: deployer,
+            proxyContract: "OpenZeppelinTransparentProxy",
+            execute: {
+                init: {
+                    methodName: "initialize",
+                    args: [marketRegistry.address],
+                },
+            },
+        },
+    })
+    log("# Orderbook contract deployed at address:", orderbookContract.address)
+    log("#########################")
+
+    if (!isDevelopmentChain(chainId)) {
+        verify(orderbookContract.address, [])
+    }
+}
+
+export default deploy
+deploy.tags = ["orderbook", "all"]
+deploy.dependencies = ["marketreg"]
