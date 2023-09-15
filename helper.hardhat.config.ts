926f1278d34983512b2c5bbaeea7c8de1c58340c
Fran <franp.dev@protonmail.com>
2023-09-15 07:51:31 +0100

    Dev market registry (#2)

--- helper.hardhat.config.ts
+++ helper.hardhat.config.ts
@@ -4,6 +4,8 @@ export const GOERLI_CHAINID = 5
 interface ContractArguments {
     name: string
     usdc?: string
+    veth?: string
+    ethusdPriceFeed?: string
 }
 interface ConfigHelper {
     [chainId: number]: ContractArguments
@@ -12,9 +14,24 @@ interface ConfigHelper {
 export const networkConfigHelper: ConfigHelper = {
     5: {
         name: "goerli",
+        usdc: "",
+        ethusdPriceFeed: "",
+    },
+    10: {
+        name: "optimism mainnet",
+        usdc: "0x7F5c764cBc14f9669B88837ca1490cCa17c31607",
+        ethusdPriceFeed: "0x13e3Ee699D1909E989722E753853AE30b17e08c5",
+    },
+    420: {
+        name: "Optimism Goerli Testnet",
+        usdc: "0x5f1C3c9D42F531975EdB397fD4a34754cc8D3b71",
+        ethusdPriceFeed: "0x57241A37733983F97C4Ab06448F244A1E0Ca0ba8",
     },
     31337: {
         name: "hardhat",
+        usdc: "",
+        ethusdPriceFeed: "",
+        veth: "",
     },
 }
 
